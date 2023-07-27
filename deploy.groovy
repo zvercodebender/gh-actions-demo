49221f9b2b48f08a55823ac433e4f7bc783d870b
Logan Donley <ldonley@cloudbees.com>
2023-07-27 10:00:34 -0400

    Delete deploy.groovy

--- deploy.groovy
+++
@@ -1,9 +0,0 @@
-def imageTag = args.imageTag
-
-runProcess (
-  applicationName: "Simple Insurance",
-  processName: "Deploy Application",
-  projectName: "ldonley Demo",
-  environmentName: "Insurance QA",
-  actualParameter: [ imageTag: imageTag ]
-)
